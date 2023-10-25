361fe2a968e6ad34c106d7aaf5a3f151c829bafe
George-Okumu <gokumu12@gmail.com>
2023-10-25 09:01:18 +0300

    Add @repository annotation to repository interface classes.

--- src/main/java/com/carinfomanager/carinfomanager/repository/CarRepository.java
+++ src/main/java/com/carinfomanager/carinfomanager/repository/CarRepository.java
@@ -2,6 +2,8 @@ package com.carinfomanager.carinfomanager.repository;
 
 import com.carinfomanager.carinfomanager.models.Car;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface CarRepository extends JpaRepository<Car, Integer> {
 }
