b41fe1aafbc2e42f67b368c9f65b8d63e46177a8
George-Okumu <gokumu12@gmail.com>
2023-10-27 20:21:28 +0300

    created script

---
+++ script.sh
@@ -0,0 +1,4 @@
+sudo chown openlink:openlink IdeaProjects/carinfomanager/target/carinfomanager-0.0.1-SNAPSHOT.jar
+
+sudo mv IdeaProjects/carinfomanager/target/carinfomanager-0.0.1-SNAPSHOT.jar /srv/
+
