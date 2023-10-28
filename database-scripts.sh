37ee04302c277af40ee0a36fd32156630112d425
George-Okumu <gokumu12@gmail.com>
2023-10-28 17:49:06 +0300

    More explanation on db scripts

--- database-scripts.sh
+++ database-scripts.sh
@@ -1,3 +1,5 @@
+# This script is for local,
+# In the properties file, I have used credentials for railway mysql connection
 CREATE DATABASE carmanager;
-CREATE user "carmanage" IDENTIFIED BY "Carinfomanage@2023"
-GRANT ALL ON carmanager.* TO 'carmanage'@'%'
+CREATE user "carmanage" IDENTIFIED BY "Carinfomanage@2023";
+GRANT ALL ON carmanager.* TO 'carmanage'@'your_local_host';
